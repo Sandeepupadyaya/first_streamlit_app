8c9dd4c68f1608bcc836afdf1c63a23ca4a253f7
Sandeepupadyaya <66593351+Sandeepupadyaya@users.noreply.github.com>
2023-05-26 09:49:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Aaloo bonda')
+streamlit.text('Idlli Sambar')
+streamlit.text('Upma')
